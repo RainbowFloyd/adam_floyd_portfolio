a44237b81307abf6c9ebfa71dae511770b4f2b4c
Adam-Floyd <rainbowfloyd911@gmail.com>
2019-10-16 09:06:32 -0400

    Add content to introduction component

---
+++ src/Components/Introduction/Introduction.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Introduction = () => {
+	return (
+		<div>
+			<div>Adam Floyd</div>
+			<div>"Introduction Text"</div>
+		</div>
+	)
+}
+
+export default Introduction
